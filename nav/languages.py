283acd109acbd53d59ed7bb49ad5ac44b92d3ef7
Andre <andre.dos.anjos@gmail.com>
2010-04-15 11:57:34 +0200

    Include a few standard translations for the languages I use the most

---
+++ nav/languages.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.dos.anjos@gmail.com>
+# Thu 15 Apr 2010 11:46:00 AM CEST 
+
+"""A list of languages for the translations.
+"""
+
+# This is defined here as a do-nothing function
+gettext_noop = lambda s: s
+
+gettext_noop('English')
+gettext_noop('Spanish')
+gettext_noop('Argentinean Spanish')
+gettext_noop('French')
+gettext_noop('Portuguese')
+gettext_noop('Brazilian Portuguese')
