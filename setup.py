83e720f7cc9930b32e387c2d0efa0582fcc819b4
Andre Anjos <andre.dos.anjos@gmail.com>
2014-06-29 12:32:38 +0200

    Django 1.6 compatibility changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 # Created by Andre Anjos <andre.dos.anjos@cern.ch>
-# Seg 14 Set 2009 14:42:06 CEST 
+# Seg 14 Set 2009 14:42:06 CEST
 
 """Installation instructions for djangoogle
 """
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = "django-nav",
-    version = "0.3.2", 
+    version = "0.3.2",
     packages = find_packages(),
 
     # we also need all translation files and templates
@@ -26,9 +26,9 @@ setup(
     zip_safe=False,
 
     install_requires = [
-      'PIL',
+      'pillow',
       'docutils',
-      
+
       # mine
       'django-order',
       ],
@@ -43,6 +43,6 @@ setup(
     license = "PSF",
     keywords = "django menu",
     url = "",   # project home page, if any
-    
+
 )
 
